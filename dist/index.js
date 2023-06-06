1f1c114c7ad70abb93afad1943c41d49a182a264
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 12:52:08 +0530

    index.html yarn build

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
-const analytics_1 = require("@vercel/analytics");
 dotenv_1.default.config({ path: path_1.default.join(__dirname, "..", ".env") });
 const PORT = process.env.PORT || 8080;
 const app = (0, express_1.default)();
@@ -18,11 +17,7 @@ app.use((0, morgan_1.default)("dev"));
 app.use((0, cors_1.default)());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
-// Routes
-app.get("/", (req, res, next) => {
-    (0, analytics_1.inject)();
-    res.status(200).json({ message: "Working" });
-});
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public")));
 app.use((req, res, next) => {
     return next(new http_errors_1.default.NotFound(`The requested url ${req.url} was not found.`));
 });
