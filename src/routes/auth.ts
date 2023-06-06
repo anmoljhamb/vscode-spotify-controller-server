8c3c98f0f52dd02cbd9663e339cfb0a5f31e2242
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 14:09:55 +0530

    refreshToken route added

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { spotifyApi } from "../utils";
+import SpotifyWebApi from "spotify-web-api-node";
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from "../constants";
 
 export const authRouter = express.Router();
 
@@ -13,6 +15,23 @@ authRouter.get("/grant", async (req, res, next) => {
         const resp = await spotifyApi.authorizationCodeGrant(code as string);
         return res.status(200).json({ ...resp.body });
     } catch (e) {
+        next(e);
+    }
+});
+
+authRouter.post("/refreshToken", async (req, res, next) => {
+    try {
+        const { refreshToken } = req.body;
+        const _spotifyApi = new SpotifyWebApi({
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            redirectUri: REDIRECT_URI,
+            refreshToken,
+        });
+        const resp = await _spotifyApi.refreshAccessToken();
+        return res.status(200).json({ ...resp.body });
+    } catch (e) {
+        // console.log(e)
         next(e);
     }
 });
