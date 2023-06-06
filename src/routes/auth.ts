a04ef779720be184c96f2ed65a4f6b519e2e2ddd
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 13:46:43 +0530

    /grant route added

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import { spotifyApi } from "../utils";
 
 export const authRouter = express.Router();
 
 authRouter.get("/", (req, res, next) => {
     return res.status(200).json({ message: "Working" });
+});
+
+authRouter.get("/grant", async (req, res, next) => {
+    try {
+        const { code } = req.query;
+        const resp = await spotifyApi.authorizationCodeGrant(code as string);
+        return res.status(200).json({ ...resp.body });
+    } catch (e) {
+        next(e);
+    }
 });
