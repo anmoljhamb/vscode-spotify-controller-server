001025a70138789f718622df825c4a2ab4649b51
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 12:18:17 +0530

    basic server created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("hello world!");
+import path from "path";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import createHttpErrors from "http-errors";
+import express, { NextFunction, Request, Response } from "express";
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// 3rd party middlewares.
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res, next) => {
+    res.status(200).json({ message: "Working" });
+});
+
+app.use((req, res, next) => {
+    return next(
+        new createHttpErrors.NotFound(
+            `The requested url ${req.url} was not found.`
+        )
+    );
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof createHttpErrors.HttpError) {
+        return res.status(err.statusCode).json({ message: err.message });
+    }
+    return res.sendStatus(500);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on the url *:${PORT}`);
+});
