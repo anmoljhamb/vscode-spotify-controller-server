ff45fd3b0551ca1a90491578de0499f7294a93ba
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 12:37:49 +0530

    vercel inject

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import createHttpErrors from "http-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { inject } from "@vercel/analytics";
 
-inject();
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const PORT = process.env.PORT || 8080;
@@ -38,6 +37,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     return res.sendStatus(500);
 });
 
+inject();
 app.listen(PORT, () => {
     console.log(`Listening on the url *:${PORT}`);
 });
